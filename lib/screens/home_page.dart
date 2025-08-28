d9c774fbd2a57c2f854faf4f49b296b36442973a
wranjali20 <wranjali20@gmail.com>
2025-08-28 21:28:45 +0000

    update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,107 +1,12 @@
-
+import 'dart:ffi';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
-
-//blueprint for task
-class Task {
-  final String id;
-  final String name;
-  final bool completed;
-
-  Task({required this.id, required this.name, required this.completed});
-
-  factory Task.fromMap(String id, Map<String, dynamic> data) {
-    return Task(
-      id: id,
-      name: data['name'] ?? '',
-      completed: data['completed'] ?? false,
-    );
-  }
-}
-
-//Define a Task Service to handle Firestore operationss
-class TaskService {
-  //Firestore instance in an alias
-  final FirebaseFirestore db = FirebaseFirestore.instance;
-
-  //Future that returns a list of tasks using factory method defined in Task class
-  Future<List<Task>> fetchTasks() async {
-    //call get to retrieve all of the documents inside the collection
-    final snapshot = await db.collection('tasks').orderBy('timestamp').get();
-    //snapshot of all documents is being mapped to factory object template
-    return snapshot.docs
-        .map((doc) => Task.fromMap(doc.id, doc.data()))
-        .toList();
-  }
-
-  //another asynchronous Future to add tasks to the firestore
-  Future<String> addTask(String name) async {
-    final newTask = {
-      'name': name,
-      'completed': false,
-      'timestamp': FieldValue.serverTimestamp(),
-    };
-    final docRef = await db.collection('tasks').add(newTask);
-    return docRef.id;
-  }
-
-  //update task future
-  Future<void> updateTask(String id, bool completed) async {
-    await db.collection('tasks').doc(id).update({'completed': completed});
-  }
-
-  //Future is going to delete tasks
-  Future<void> deleteTasks(String id) async {
-    await db.collection('tasks').doc(id).delete();
-  }
-}
-
-//create a task provider to manage state
-class TaskProvider extends ChangeNotifier {
-  final TaskService taskService = TaskService();
-  List<Task> tasks = [];
-
-  //populates tasks list/array with documents from database
-  //notifies the root provider of stateful change
-  Future<void> loadTasks() async {
-    tasks = await taskService.fetchTasks();
-    notifyListeners();
-  }
-
-  Future<void> addTask(String name) async {
-    //check to see if name is not empty or null
-    if (name.trim().isNotEmpty) {
-      //add the trimmed task name to the database
-      final id = await taskService.addTask(name.trim());
-      //adding the task name to the local list held in memory
-      tasks.add(Task(id: id, name: name, completed: false));
-      notifyListeners();
-    }
-  }
-
-  Future<void> updateTask(int index, bool completed) async {
-    //uses array index to find tasks
-    final task = tasks[index];
-    //update the task collection in the database by id, using bool for completed
-    await taskService.updateTask(task.id, completed);
-    //updating the local tasks list
-    tasks[index] = Task(id: task.id, name: task.name, completed: completed);
-    notifyListeners();
-  }
-
-  Future<void> removeTask(int index) async {
-    //uses array index  to find tasks
-    final task = tasks[index];
-    //delete the task from the collection
-    await taskService.deleteTasks(task.id);
-    //remote the task from the list in memory
-    tasks.removeAt(index);
-    notifyListeners();
-  }
-}
+import 'package:myapp/models/tasks.dart';
+import 'package:myapp/services/task_service.dart';
+import 'package:myapp/providers/task_provider.dart';
 
 class Home_Page extends StatefulWidget {
   const Home_Page({super.key});
@@ -143,20 +48,37 @@ class _Home_PageState extends State<Home_Page> {
       ),
       body: Column(
         children: [
-          TableCalendar(
-            calendarFormat: CalendarFormat.month,
-            focusedDay: DateTime.now(),
-            firstDay: DateTime(2025),
-            lastDay: DateTime(2026),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TableCalendar(
+                    calendarFormat: CalendarFormat.month,
+                    focusedDay: DateTime.now(),
+                    firstDay: DateTime(2025),
+                    lastDay: DateTime(2026),
+                  ),
+                  Consumer<TaskProvider>(
+                    builder: (context, taskProvider, child) {
+                      return buildTaskItem(
+                        taskProvider.tasks,
+                        taskProvider.removeTask,
+                        taskProvider.updateTask,
+                      );
+                    },
+                  ),
+                  Consumer<TaskProvider>(
+                    builder: (context, taskProvider, child) {
+                      return buildAddTaskSection(nameController, () async {
+                        await taskProvider.addTask(nameController.text);
+                        nameController.clear();
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
-          Consumer<TaskProvider>( builder:(context, taskProvider, child){
-            return buildAddTaskSection(
-              nameController,
-              () async {
-                await taskProvider.addTask(nameController.text);
-                nameController.clear();
-              });
-          })
         ],
       ),
       drawer: Drawer(),
@@ -178,12 +100,62 @@ Widget buildAddTaskSection(nameController, addTask) {
               decoration: const InputDecoration(
                 labelText: 'Add Task',
                 border: OutlineInputBorder(),
-              )
-          )
+              ),
+            ),
           ),
         ),
         ElevatedButton(onPressed: addTask, child: Text('Add Task')),
       ],
     ),
+  );
+}
+
+//Widget that displays the task items on the UI
+Widget buildTaskItem(
+  List<Task> tasks,
+  Function(int) removeTasks,
+  Function(int, bool) updateTask,
+) {
+  return ListView.builder(
+    shrinkWrap: true,
+    physics: const NeverScrollableScrollPhysics(),
+    itemCount: tasks.length,
+    itemBuilder: (context, index) {
+      final task = tasks[index];
+      final isEven = index % 2 == 0;
+
+      return Padding(
+        padding: EdgeInsets.all(1.0),
+        child: ListTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          tileColor: isEven ? Colors.blue : Colors.green,
+          leading: Icon(
+            task.completed ? Icons.check_circle : Icons.circle_outlined,
+          ),
+          title: Text(
+            task.name,
+            style: TextStyle(
+              decoration: task.completed ? TextDecoration.lineThrough : null,
+              fontSize: 22,
+            ),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Checkbox(
+                value: task.completed,
+                onChanged: (value) => {updateTask(index, value!)},
+              ),
+              IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () => removeTasks(index),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
   );
 }
